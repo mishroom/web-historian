dd22eadd9f3234ebb616b81aae3916206e48ca97
mishroom <mishall.afzal@gmail.com>
2017-12-20 18:44:15 -0800

    progress

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,22 +2,58 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 var fs = require('fs');
-//var thisPage = require('./public/index.html');
+
+
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10,
+  'content-type': 'application/json'
+};
+
+var sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+};
+
+var collectData = function(request, callback) {
+  var data = "";
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+  request.on('end', function() {
+    callback(JSON.parse(data));
+  });
+};
+
+var actions = {
+  'GET': function(request, response) {
+    console.log('you made a GET request');
+  },
+  'POST': function(request, response) {
+    console.log('you made a POST request');
+  },
+  'OPTIONS': function(request, response) {
+    console.log('you made an OPTIONS request');
+  }
+};
 
 exports.handleRequest = function (req, res) {
-  fs.readFile('./web/public/index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-   //  console.log('the req is ');
-   // console.log(req);
-    res.write(data);
-    res.end();
-  });
-  // if (req.method === 'GET') {
-  //   // res.write(this.html);
-  
-  //   console.log('you made a GET request');
-  // } else {
-  //   console.log('you made some other type of request');
-  // // }
-  // res.end(archive.paths.list);
+  var action = actions[req.method];
+  if (action) {
+    action(req, res);
+    fs.readFile('./web/public/index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+    // fs.readFile('./web/public/styles.css', function(err, data) {
+    //   res.writeHead(200, {'Content-Type': 'text/css'});
+    //   res.write(data);
+      
+    // });
+  } 
 };
+
